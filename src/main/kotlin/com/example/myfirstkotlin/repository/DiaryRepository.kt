e1149cbd45c2d58dc6979fbb89d7b9ec482768ca
kannna5296 <kannnahacks5296@gmail.com>
2022-02-22 23:05:10 +0900

    メモ追加

--- src/main/kotlin/com/example/myfirstkotlin/repository/DiaryRepository.kt
+++ src/main/kotlin/com/example/myfirstkotlin/repository/DiaryRepository.kt
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
+//TODO インターフェースがJpaに依存するのは辞めたい
+
 @Repository
 @Transactional
 interface DiaryRepository : JpaRepository<Diary, Int>
