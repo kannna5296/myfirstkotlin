8fd915b99c6a75314acd1c2d952f7b44c3ec6451
kannna5296 <kannnahacks5296@gmail.com>
2022-02-22 22:45:15 +0900

    フォーマッタ

--- src/main/kotlin/com/example/myfirstkotlin/entity/Diary.kt
+++ src/main/kotlin/com/example/myfirstkotlin/entity/Diary.kt
@@ -1,7 +1,11 @@
 package com.example.myfirstkotlin.entity
 
 import java.sql.Timestamp
-import javax.persistence.*
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
 
 @Entity
 @Table(name = "diary")
